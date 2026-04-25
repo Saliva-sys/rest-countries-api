cc309cf62c7add67d219702f228dcfed50e40121
Saliva-sys <aweidlichova@gmail.com>
2026-04-25 17:24:25 +0200

    Cleanup and structure setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
+import RestCountries from "./RestCountries";
+
 function App() {
   return (
-    <div className="bg-slate-100 min-h-screen flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-600">
-        Tailwind je nastavený!
-      </h1>
-      <p className="text-gray-500 mt-4">
-        Práve si dokončila 6. krok tvojho ťaháka.
-      </p>
-    </div>
-  )
+    <main>
+      <RestCountries />
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/RestCountries.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="bg-slate-100 min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind je nastavený!
+      </h1>
+      <p className="text-gray-500 mt-4">
+        Práve si dokončila 6. krok tvojho ťaháka.
+      </p>
+    </div>
+  )
+}
+
+export default App
